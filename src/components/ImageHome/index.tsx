6620c97aff5139616c53885f67b8b6e66e691efb
João Paulo <joao_paulinh@hotmail.com>
2022-05-06 16:36:56 -0300

    Update index.tsx

--- src/components/ImageHome/index.tsx
+++ src/components/ImageHome/index.tsx
@@ -3,19 +3,24 @@ import { RootState } from '../../store'
 
 import { Container, Star } from './styles'
 import { BiRightArrow } from 'react-icons/bi'
-import { getOneMovieRequest } from '../../store/ducks/moviesRepository/actions'
+import {
+  getOneMovieRequest,
+  loadRequest,
+} from '../../store/ducks/moviesRepository/actions'
 import { useNavigate } from 'react-router'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export function ImageHome() {
   const { data } = useSelector((state: RootState) => state.moviesRepository)
-  const [firstMovie, setFirstMovie] = useState(data)
+
+  const navigate = useNavigate()
+  const IMG_API = 'https://image.tmdb.org/t/p/w1280'
 
   const dispatch = useDispatch()
 
-  const navigate = useNavigate()
-
-  const IMG_API = 'https://image.tmdb.org/t/p/w1280'
+  useEffect(() => {
+    dispatch(loadRequest(1))
+  }, [])
 
   function voteStar(verificationNumber: number) {
     const numberVote = Number(data[0].vote_average) * 10
@@ -43,18 +48,18 @@ export function ImageHome() {
 
   return (
     <Container>
-      <img src={IMG_API + firstMovie[0].backdrop_path} alt="" />
+      <img src={IMG_API + data[0].backdrop_path} alt="" />
       <div>
-        <h2>{firstMovie[0].title}</h2>
+        <h2>{data[0].title}</h2>
         <div>
-          <span>{firstMovie[0].vote_average}</span>
+          <span>{data[0].vote_average}</span>
           <Star className={voteStar(20)}>&#9733;</Star>
           <Star className={voteStar(40)}>&#9733;</Star>
           <Star className={voteStar(60)}>&#9733;</Star>
           <Star className={voteStar(80)}>&#9733;</Star>
           <Star className={voteStar(100)}>&#9733;</Star>
         </div>
-        <button onClick={() => handleSelectMovie(firstMovie[0].id)}>
+        <button onClick={() => handleSelectMovie(data[0].id)}>
           Ver informações <BiRightArrow className="arrow" />
         </button>
       </div>
